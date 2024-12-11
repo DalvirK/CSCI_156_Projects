a8abce6c5d084a97ee9cc3e8c71a0a031ef55ca7
Devin <31043454+dceteves@users.noreply.github.com>
2024-12-11 14:23:14 -0800

    Added basic whisper command functionality
    - Refactor client_sockets dictionary hashing to take in tuple (client_socket, addr) as key

--- T2/BRP_server.py
+++ T2/BRP_server.py
@@ -91,32 +91,59 @@ def moveClient(client_socket, addr, room_name):
         return "Room does not exist"
 
     # Remove client from client's room and broadcast to that room
-    rooms[client_sockets[addr]['room']].remove((client_socket, addr))
-    broadcast(f"{client_sockets[addr]['name']} has left the room", client_sockets[addr]['room'])
+    rooms[client_sockets[(client_socket, addr)]['room']].remove((client_socket, addr))
+    broadcast(f"{client_sockets[(client_socket, addr)]['name']} has left the room", client_sockets[(client_socket, addr)]['room'])
 
     # Add client to new room and change client's info
-    client_sockets[addr]['room'] = room_name
+    client_sockets[(client_socket, addr)]['room'] = room_name
     rooms[room_name].append((client_socket, addr))
 
     # Broadcast to new room
-    broadcast(f"{client_sockets[addr]['name']} has joined the room", room_name, client_socket)
+    broadcast(f"{client_sockets[(client_socket, addr)]['name']} has joined the room", room_name, client_socket)
 
     # Send message to client that they have successfully joined the new room
     return f"You have joined room {room_name}"
 
-def broadcast(message, room_name, client_except=None):
+def broadcast(message, room_name, clients_except=None):
     """Sends a message to all clients in a room
 
     Args:
         message (string): Message to be broadcasted to
         room (string): name of the room to be broadcasted in 
-        client_except (socket): Client socket which will not get the message (optional)
+        clients_except (list[socket]): Client socket which will not get the message (optional)
     """
+
+    if clients_except is None:
+        clients_except = []
 
     # Using room_name as key iterate through each client in the room
     for client, _ in rooms[room_name]:
-        if client != client_except:
+        if client not in clients_except:
             client.send(message.encode('utf-8'))
+
+def whisper(client_socket, addr, split_message):
+    """Privately sends a message between two client sockets
+    Args:
+        client_socket (socket): the client socket that wants to privately send a message
+        addr (string): address of the client socket 
+        split_message (list[str]): original message split into a list by spaces
+    """
+    recipient_name = split_message[1]
+    recipient_socket = None
+    client_name = client_sockets[(client_socket, addr)]['name']
+    message = ' '.join(split_message[2:])
+
+    for socket, info in client_sockets.items():
+        if info['name'] == recipient_name:
+            recipient_socket = socket[0]
+            break
+
+    if recipient_socket is None:
+        client_socket.send(f"User {recipient_name} not found".encode('utf-8'))
+    elif recipient_socket == client_socket:
+        client_socket.send(f"You cannot whisper to yourself".encode('utf-8'))
+    else:
+        recipient_socket.send(f"From {client_name}: {message}".encode('utf-8'))
 
 def handle_command(client_socket, addr, split_message):
     """handle messages from the client that start with '!' (command)
@@ -129,22 +156,24 @@ def handle_command(client_socket, addr, split_message):
     split_message[0] = split_message[0].lower()
 
     if split_message[0] == 'room':
-        client_socket.send(client_sockets[addr]['room'].encode('utf-8'))
+        client_socket.send(client_sockets[(client_socket, addr)]['room'].encode('utf-8'))
     elif split_message[0] == 'list':
         client_socket.send(listRooms().encode('utf-8'))
     elif split_message[0] == 'join' and len(split_message) > 1:
         client_socket.send(moveClient(client_socket, addr, split_message[1]).encode('utf-8'))
-    elif split_message[0] == 'create' and len(split_message) > 1 and client_sockets[addr]['type']:
+    elif split_message[0] == 'create' and len(split_message) > 1 and client_sockets[(client_socket, addr)]['type']:
         client_socket.send(createRoom(split_message[1]).encode('utf-8'))
-    elif split_message[0] == 'close' and len(split_message) > 1 and client_sockets[addr]['type']:
+    elif split_message[0] == 'close' and len(split_message) > 1 and client_sockets[(client_socket, addr)]['type']:
         closeRoomThreaded(client_socket, addr, split_message[1:])
-    elif split_message [0] == 'closeall' and len(split_message) == 1 and client_sockets[addr]['type']:
+    elif split_message [0] == 'closeall' and len(split_message) == 1 and client_sockets[(client_socket, addr)]['type']:
         allrooms = list(rooms.keys())[1:]
         closeRoomThreaded(client_socket, addr, allrooms)
-    elif split_message[0] == 'broadcast' and len(split_message) > 1 and client_sockets[addr]['type']:
+    elif split_message[0] == 'broadcast' and len(split_message) > 1 and client_sockets[(client_socket, addr)]['type']:
         for room in rooms:
-            broadcast(f"[BROADCAST] {client_sockets[addr]['name']}: " + ' '.join(split_message[1:]), room, client_socket)
+            broadcast(f"[BROADCAST] {client_sockets[(client_socket, addr)]['name']}: " + ' '.join(split_message[1:]), room, [client_socket])
         client_socket.send("Message broadcasted to all rooms".encode('utf-8'))
+    elif split_message[0] == 'whisper' and len(split_message) > 2:
+        whisper(client_socket, addr, split_message)
     else:
         client_socket.send("Unknown Command".encode('utf-8'))
 
@@ -158,17 +187,17 @@ def handle_message(client_socket, addr, message):
 
     """
 
-    if client_sockets[addr]['type']:
+    if client_sockets[(client_socket, addr)]['type']:
         client_type = "(INSTRUCTOR)" 
     else:
         client_type = "(STUDENT)"
 
-    output = f"{client_type} {client_sockets[addr]['name']}: {message}"
+    output = f"{client_type} {client_sockets[(client_socket, addr)]['name']}: {message}"
 
-    print(f"[{client_sockets[addr]['room']}] {output}")
+    print(f"[{client_sockets[(client_socket, addr)]['room']}] {output}")
 
     if message[0] != command_prefix:
-        broadcast(output, client_sockets[addr]['room'], client_socket);
+        broadcast(output, client_sockets[(client_socket, addr)]['room'], [client_socket]);
     else:
         handle_command(client_socket, addr, message[1:].split())
     
@@ -185,9 +214,9 @@ def handle_client(client_socket, addr):
         if message:
             if message == "!quit":
                 connected = False
-                rooms[client_sockets[addr]['room']].remove((client_socket, addr))
+                rooms[client_sockets[(client_socket, addr)]['room']].remove((client_socket, addr))
                 for room in rooms:
-                    broadcast(f"{client_sockets[addr]['name']} has left", room)
+                    broadcast(f"{client_sockets[(client_socket, addr)]['name']} has left", room)
                 client_socket.close()
             else:
                 handle_message(client_socket, addr, message)
@@ -202,7 +231,7 @@ def start_server():
         client_socket, addr = server_socket.accept()
 
         client_info = client_socket.recv(1024).decode('utf-8')
-        client_sockets[addr] = {
+        client_sockets[(client_socket, addr)] = {
                 'type': int(client_info[0]),
                 'name': client_info[1:],
                 'room': 'main'
@@ -210,9 +239,9 @@ def start_server():
 
         rooms['main'].append( (client_socket, addr) )
 
-        print(f"{client_sockets[addr]} connected from {addr}")
+        print(f"{client_sockets[(client_socket, addr)]} connected from {addr}")
         for room in rooms:
-            broadcast(f"{client_sockets[addr]['name']} has joined main room", room)
+            broadcast(f"{client_sockets[(client_socket, addr)]['name']} has joined main room", room)
 
         client_handler = threading.Thread(target=handle_client, args=(client_socket, addr))
 
